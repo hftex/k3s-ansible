b3e95599bc1da38409ddac980e35ebb1a5cb886d
djolez <djordje.simic@fidexx.io>
2021-04-14 11:56:00 +0200

    more cleanup tasks, ECR creds script

---
+++ post_install_scripts/ecr.sh
@@ -0,0 +1,26 @@
+# From https://stackoverflow.com/a/55658863
+# Additional info on https://github.com/k3s-io/k3s/issues/1427
+
+ACCOUNT=980925581578
+REGION=us-east-1
+SECRET_NAME=${REGION}-ecr-registry
+EMAIL=no@email.com
+
+#
+#
+
+TOKEN=`aws ecr --region=$REGION get-authorization-token --output text \
+    --query authorizationData[].authorizationToken | base64 -d | cut -d: -f2`
+
+#
+#  Create or replace registry secret
+#
+
+
+kubectl delete secret --ignore-not-found $SECRET_NAME
+kubectl create secret docker-registry $SECRET_NAME \
+    --namespace=demo \
+    --docker-server=https://${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com \
+    --docker-username=AWS \
+    --docker-password="${TOKEN}" \
+    --docker-email="${EMAIL}"
